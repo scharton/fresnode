cb8a4a546e4ae51064cdffa8cbc69ba6acd6a396
Derek Scharton <dscharton@financialengines.com>
2015-09-15 17:38:39 -0700

    initial node app files

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var app = require('express')();
+app.get('/', function(req, res) {
+	res.send('Hello World');
+});
+
+var server = app.listen(3000, function () {
+
+var host = server.address().address;
+var port = server.address().port;
+console.log('Listing on http://%s:%s', host, port);
+});
+
+
