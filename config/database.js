57ae4bfa085dba03565665a2e6b9507972bd614e
Derek Scharton <dscharton@financialengines.com>
2015-10-13 21:13:12 -0700

    Updates based directly on content from tutorial https://scotch.io/tutorials/easy-node-authentication-setup-and-local

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://fresnode:bitwise@ds051523.mongolab.com:51523/fresnode' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url': 'mongodb://fresnode:bitwise@ds051523.mongolab.com:51523/fresnode' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
--- server.js
+++ server.js
@@ -2,25 +2,25 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 
-var morgan       = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -30,7 +30,7 @@ app.use(bodyParser()); // get information from html forms
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({secret: 'ilovescotchscotchyscotchscotch'})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -45,57 +45,68 @@ console.log('The magic happens on port ' + port);
 
 
 // Temporary while troubleshooting routes.js
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
-    app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
+// =====================================
+// HOME PAGE (with login links) ========
+// =====================================
+app.get('/', function (req, res) {
+    res.render('index.ejs'); // load the index.ejs file
+});
+
+// =====================================
+// LOGIN ===============================
+// =====================================
+// show the login form
+app.get('/login', function (req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('login.ejs', {message: req.flash('loginMessage')});
+});
+
+// process the login form
+// app.post('/login', do all our passport stuff here);
+app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
+
+// =====================================
+// SIGNUP ==============================
+// =====================================
+// show the signup form
+app.get('/signup', function (req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', {message: req.flash('signupMessage')});
+});
+
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+}));
+
+// process the signup form
+// app.post('/signup', do all our passport stuff here);
+
+// =====================================
+// PROFILE SECTION =====================
+// =====================================
+// we will want this protected so you have to be logged in to visit
+// we will use route middleware to verify this (the isLoggedIn function)
+app.get('/profile', isLoggedIn, function (req, res) {
+    res.render('profile.ejs', {
+        user: req.user // get the user out of session and pass to template
     });
+});
 
-    // =====================================
-    // LOGIN ===============================
-    // =====================================
-    // show the login form
-    app.get('/login', function(req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('login.ejs', { message: req.flash('loginMessage') }); 
-    });
-
-    // process the login form
-    // app.post('/login', do all our passport stuff here);
-
-    // =====================================
-    // SIGNUP ==============================
-    // =====================================
-    // show the signup form
-    app.get('/signup', function(req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
-    });
-
-    // process the signup form
-    // app.post('/signup', do all our passport stuff here);
-
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
-            user : req.user // get the user out of session and pass to template
-        });
-    });
-
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
+// =====================================
+// LOGOUT ==============================
+// =====================================
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
